27dd593790c0187076dd26c4c6e84f3cdf01ccaa
Kostas Georgiou <>
2023-12-06 10:47:33 +0200

    fix: build template script use build server nuget if exists

--- Scripts/build_template.bat
+++ Scripts/build_template.bat
@@ -1,3 +1,9 @@
+SET nugetpath="nuget"
+where /q nuget
+IF ERRORLEVEL 1 (
+    SET nugetpath="%~dp0nuget.exe"
+)
+
 if (%1) == () goto blank
 "%~dp0nuget.exe" pack "%~dp0templates.nuspec"  -OutputDirectory "%~dp0..\Output" -Version %1
 goto end
