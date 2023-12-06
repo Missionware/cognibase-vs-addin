95e3bd9415b8f6915917c942d4732ef43e9c5150
Kostas Georgiou <>
2023-12-06 10:50:23 +0200

    fix: nuget on build template

--- Scripts/build_template.bat
+++ Scripts/build_template.bat
@@ -5,8 +5,8 @@ IF ERRORLEVEL 1 (
 )
 
 if (%1) == () goto blank
-"%~dp0nuget.exe" pack "%~dp0templates.nuspec"  -OutputDirectory "%~dp0..\Output" -Version %1
+%nugetpath% pack "%~dp0templates.nuspec"  -OutputDirectory "%~dp0..\Output" -Version %1
 goto end
 :blank
-"%~dp0nuget.exe" pack "%~dp0templates.nuspec"  -OutputDirectory "%~dp0..\Output"
+%nugetpath% pack "%~dp0templates.nuspec"  -OutputDirectory "%~dp0..\Output"
 :end
