9479f2fa744efa41cc55fd22455f2323ba62d42e
Kostas Georgiou <>
2024-01-10 11:09:43 +0200

    fix: overview

---
+++ Assets/readmeheader_vsix.md
@@ -0,0 +1,3 @@
+﻿# About this package
+
+This Extension is part of the Cognibase Platform. It contains project templates to bootstrap Cognibase .Net projects.
--- Scripts/vsix_convert_readme_to_html.bat
+++ Scripts/vsix_convert_readme_to_html.bat
@@ -1,3 +1,5 @@
 "%~dp0pandoc.exe" -s -o "%~dp0..\Assets\readme.md" "%~dp0..\Assets\readmeheader.md" "%~dp0..\Assets\readmefooter.md" 
 
+"%~dp0pandoc.exe" -s -o "%~dp0..\Assets\readme_vsix.md" "%~dp0..\Assets\readmeheader_vsix.md" "%~dp0..\Assets\readmefooter.md" 
+
 "%~dp0pandoc.exe" -s -o "%~dp0..\Source\TemplatesVsix\CognibaseTemplates\GettingStarted.html" "%~dp0..\Assets\readme.md" "%~dp0..\Assets\readmefooter.md" 
