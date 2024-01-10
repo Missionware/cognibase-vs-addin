a4f3354a156e68e064af04344e204d5ac04a73f3
Kostas Georgiou <>
2024-01-10 11:10:40 +0200

    fix: readme

--- Scripts/vsix_convert_readme_to_html.bat
+++ Scripts/vsix_convert_readme_to_html.bat
@@ -2,4 +2,4 @@
 
 "%~dp0pandoc.exe" -s -o "%~dp0..\Assets\readme_vsix.md" "%~dp0..\Assets\readmeheader_vsix.md" "%~dp0..\Assets\readmefooter.md" 
 
-"%~dp0pandoc.exe" -s -o "%~dp0..\Source\TemplatesVsix\CognibaseTemplates\GettingStarted.html" "%~dp0..\Assets\readme.md" "%~dp0..\Assets\readmefooter.md" 
+"%~dp0pandoc.exe" -s -o "%~dp0..\Source\TemplatesVsix\CognibaseTemplates\GettingStarted.html" "%~dp0..\Assets\readme_vsix.md"
